3134db664839d2af51ac4f31597c00d47f2ab6ce
shmuelhoro-glitch <shmuelhoro@gmail.com>
2026-05-20 12:38:07 +0300

    Adding function signatures

---
+++ main.py
@@ -0,0 +1,39 @@
+def show_menu() -> None:
+    pass
+
+def get_user_choice() -> str:
+    pass
+
+def handle_add_soldier() -> None:
+    pass
+
+
+def handle_add_soldier() -> None:
+    pass
+
+
+def handle_remove_soldier() -> None:
+    pass
+
+
+def handle_view_soldiers() -> None:
+    pass
+
+
+def handle_add_duty() -> None:
+    pass
+
+
+def handle_update_duty_status() -> None:
+    pass
+
+
+def handle_view_soldier_duties() -> None:
+    pass
+
+
+def main() -> None:
+    pass
+
+
+
