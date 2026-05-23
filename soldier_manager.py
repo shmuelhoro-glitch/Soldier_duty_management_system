ed91171a09b118a285345a5c6b5bce757f39818f
shmuelhoro-glitch <shmuelhoro@gmail.com>
2026-05-24 02:00:10 +0300

    Adding a docstring to utils and implementing the soldier_manager functions

--- soldier_manager.py
+++ soldier_manager.py
@@ -1,9 +1,28 @@
+from data import soldiers_data
+from utils import find_soldier_by_id,is_valid_name
 
 def add_soldier(soldier_id: int, name: str) -> None:
-    pass
+    if find_soldier_by_id(soldier_id):
+        raise ValueError("id exists")
+    elif not is_valid_name(name):
+        raise ValueError("invalid name")
+    else:
+        soldiers_data.append({"id":soldier_id,"name":name,"duties":[]})
 
 def remove_soldier(soldier_id: int) -> None:
-    pass
+    if find_soldier_by_id(soldier_id) == None:
+        raise KeyError("id isn't exists")
+    else:
+        for soldier in soldiers_data:
+            if soldier["id"] == soldier_id:
+                soldiers_data.remove(soldier)
+                break
 
 def get_all_soldiers() -> list:
-    pass
+    return soldiers_data
+
+add_soldier(19876,"dsoaop")
+add_soldier(548142,"shmuel")
+print(get_all_soldiers())
+remove_soldier(19876)
+print(get_all_soldiers())
--- utils.py
+++ utils.py
@@ -1,30 +1,33 @@
 from data import soldiers_data
 
-
+"""Check if a id is exists in the system"""
 def find_soldier_by_id(soldier_id: int) -> dict | None:
     for soldier in soldiers_data:
         if soldier["id"] == soldier_id:
             return soldier
     return None
 
+"""Searching for a shift by name in the list of shifts"""
 def find_duty_by_name(duties: list, duty_name: str) -> dict | None:
     for duty in duties:
         if duty == duty_name:
             return duty
     return None
 
+"""Checks if a status is valid"""
 def is_valid_status(status: str) -> bool:
     if status in ["pending","completed","missed"]:
         return True
     return False
 
 
+"""Checks if the name is valid (not empty)"""
 def is_valid_name(name: str) -> bool:
     if name not in [""," ",None,"  "]:
         return True
     return False
 
-
+"""Checks if a soldier has a duty with a specific name"""
 def soldier_has_duty(soldier: dict, duty_name: str) -> bool:
     for duty in soldier["duties"]:
         if duty["name"] == duty_name:
@@ -32,6 +35,7 @@ def soldier_has_duty(soldier: dict, duty_name: str) -> bool:
     return False
 
 
+"""Checks if the day is valid (not Friday or Saturday)"""
 def is_valid_day(day: str) -> bool:
     if day in ["sunday","monday","tuesday","wednesday","thursday"]:
         return True
