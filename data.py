01010dce307ce992a78de7b6f6c6d69eecd2ab08
shmuelhoro-glitch <shmuelhoro@gmail.com>
2026-05-23 23:52:03 +0300

    Implementing the functions in the utils file and adding fake data for tests

---
+++ data.py
@@ -0,0 +1,4 @@
+soldiers_data = [{"id":12345,"name":"דוד כהן","duties":[{"name": "guard duty","day": "sunday","status": "pending"}]}
+                 ,{"id":124545,"name":"משה כהן","duties":[{"name": "guard duty","day": "sunday","status": "pending"}]}]
+
+
--- utils.py
+++ utils.py
@@ -1,24 +1,38 @@
-
+from data import soldiers_data
 
 
 def find_soldier_by_id(soldier_id: int) -> dict | None:
-    pass
+    for soldier in soldiers_data:
+        if soldier["id"] == soldier_id:
+            return soldier
+    return None
 
 def find_duty_by_name(duties: list, duty_name: str) -> dict | None:
-    pass
+    for duty in duties:
+        if duty == duty_name:
+            return duty
+    return None
 
 def is_valid_status(status: str) -> bool:
-    pass
+    if status in ["pending","completed","missed"]:
+        return True
+    return False
 
 
 def is_valid_name(name: str) -> bool:
-    pass
+    if name not in [""," ",None,"  "]:
+        return True
+    return False
 
 
 def soldier_has_duty(soldier: dict, duty_name: str) -> bool:
-    pass
+    for duty in soldier["duties"]:
+        if duty["name"] == duty_name:
+            return True
+    return False
 
 
 def is_valid_day(day: str) -> bool:
-    pass
-
+    if day in ["sunday","monday","tuesday","wednesday","thursday"]:
+        return True
+    return False
